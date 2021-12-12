066f163c73ee24dc256c82c5bcd09f94f9477f76
quanghuy29 <73057345+quanghuy29@users.noreply.github.com>
2021-12-12 07:43:08 +0700

    client

---
+++ Client/Cons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Cons
+    {
+        public static int BUTTON_WIDTH = 110;
+        public static int BUTTON_HEIGHT = 110;
+
+        public static int BOARD = 3;
+    }
+}
